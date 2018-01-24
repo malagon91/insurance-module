7a1d90cad8996e2ece479d3818222a8eadf1cc5d
Unknown <miguelmalagon@outlook.com>
2018-01-23 18:07:48 -0600

    add customer files and npm task

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,8 +5,17 @@
  * Date: 1/22/18
  * Time: 6:23 PM
  */
-namespace \App\Http\Controllers;
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
 
 class MainController extends Controller{
+    public function home(){
+        return view('main.home',["name" => "new"]);
 
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,14 @@
 |
 */
 
+//Route::get('/', 'MainController@home');
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('login'));
 });
 
 Auth::routes();
+
+
 
 Route::get('/home', 'HomeController@index')->name('home');
